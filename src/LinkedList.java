6901541d1036829a8b35a12ff0fd1336283a663e
helenadouglas <helena.m.douglas02@gmail.com>
2023-10-06 14:48:43 +0200

    Linked Lists

---
+++ src/LinkedList.java
@@ -0,0 +1,18 @@
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+        private Node first;
+        private Node last;
+    }
+    //addFirst
+    public void addFirst(int item){
+        var node = new Node();
+        node.value = item;
+    }
+    //addLast
+    //deleteFirst
+    //deleteLast
+    //contains
+    //indexOf
+}
