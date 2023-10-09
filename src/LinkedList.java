38ed3447bc4564e9fefbb99845c0e2766d72f8b4
helenadouglas <helena.m.douglas02@gmail.com>
2023-10-09 12:15:18 +0200

    Linked Lists size, reverse etc

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,14 +1,17 @@
+import java.util.NoSuchElementException;
+
 public class LinkedList {
-    private class Node {
+    private static class Node {
         private int value;
         private Node next;
         public Node (int value){
             this.value = value;
         }
-
     }
     private Node first;
     private Node last;
+    private int size;
+
     private boolean isEmpty(){
         return first == null;
     }
@@ -21,9 +24,8 @@ public class LinkedList {
             node.next = node;
             first = node;
         }
+        size++;
     }
-
-
     //addLast
     public void addLast(int item){
         var node = new Node(item);
@@ -33,14 +35,9 @@ public class LinkedList {
             last.next = node;
             last = node;
         }
+        size++;
     }
-    //deleteFirst
-    public void deleteFirst (){
-        var second = first.next;
-        first.next = null;
-        first = second;
-    }
-    //deleteLast
+
     //contains
     public boolean contains (int item){
         return indexOf(item) != -1;
@@ -57,4 +54,81 @@ public class LinkedList {
         }
         return -1;
     }
+    //deleteFirst
+    public void removeFirst (){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+
+        if(first == last)
+            first = last = null;
+        else{
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+        size--;
+    }
+    //deleteLast
+    public void removeLast(){
+        if(isEmpty())
+            throw new NoSuchElementException();
+        if(first == last)
+            first = last = null;
+           else {
+            var previous = getPrevious(last);
+            last = previous; // shrink list
+            last.next = null; //removes link
+        }
+        size--;
+    }
+
+    private Node getPrevious (Node node){
+        var current = first;
+        while (current != null){
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    //size()
+    public int size(){ // runtime complexity = O(1)
+        return size;
+    }
+
+    public int[] toArray(){
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while(current != null){
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    public void reverse(){
+        if(isEmpty()) return;
+        //set the two variables
+        var previous = first;
+        var current = first.next;
+
+        //change the direction of the links
+        while (current != null){
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        //set the last field
+        last = first;
+        last.next = null;
+        //set the first field
+        first = previous;
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.Arrays;
-import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        java.util.LinkedList<String> x = new java.util.LinkedList<>();
-        x.removeFirst();
-        var list = new LinkedList<>();
-//        list.addLast(10);
-//        list.addLast(20);
-//        list.addLast(30);
-        list.removeFirst();
-        System.out.println(list.contains(10));
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        //list.removeFirst();
+        //list.removeLast();
+        list.reverse();
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
